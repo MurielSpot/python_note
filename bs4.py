bb3133a59da206f0c51488ac346fda40555bf8b4
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-02-20 00:16:32 +0800

    Update bs4.py

--- bs4.py
+++ bs4.py
@@ -10,4 +10,11 @@ html_sample='\
 <html>'
 
 soup=BeautifulSoup(html_sample,'html.parser') #如果不明确使用什么parser（剖析器），会给出警告它自己选用了一个比较好的剖析器，但是不能保证在任何环境下都能有好的结果。
+print(type(soup))
 print(soup.text)
+
+'''
+结果：
+<class 'bs4.BeautifulSoup'>
+Hello worldthis is the linkthis is link2
+'''
